caa424a1bf351e144beaeb858a423f9024e5bd8f
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2025-12-24 12:03:49 +0530

    studentsController.js commited

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -9,12 +9,14 @@ const addStudents = async (req, res) => {
     try {
         const data = req.body;
         console.log(data);
-        // const addeddata = await student.create(data);
-        const addeddata = await student.insertMany(data);
+
+        const addeddata = await student.create(data);
+        // const addeddata = await student.insertMany([data]);
         console.log(addeddata);
         res.status(201).json("data added");
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
 };
+
 export { getStudentsDetails, addStudents };

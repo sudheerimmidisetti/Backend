2d6893e164b79acabccba3212340dd5bfdcbca0a
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2025-12-31 11:09:28 +0530

    Updated studentsRouters.js

--- routers/studentsRouters.js
+++ routers/studentsRouters.js
@@ -1,11 +1,25 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { getStudentsDetails, addStudents, getStudentById, getStudentsDetailsWithFilters, updateStudents } from '../controllers/studentsController.js';
+import {
+  getStudentsDetails,
+  addStudents,
+  getStudentById,
+  getStudentsDetailsWithFilters,
+  updateStudents,
+  UpdateStudentsStatus,
+  deleteStudentById,
+  deleteStudentById2,
+  deleteStudentMany,
+} from "../controllers/studentsController.js";
 
-router.get('/get-students', getStudentsDetails);
-router.post('/add-students', addStudents);
-router.get('/get-student-byid/:userid', getStudentById); //params single
-router.get('/get-std-details-withfilter', getStudentsDetailsWithFilters); //query parameters
-router.put('/put-students/:id', updateStudents); //put method
+router.get("/get-students", getStudentsDetails);
+router.post("/add-students", addStudents);
+router.get("/get-student-byid/:userid", getStudentById); //params single
+router.get("/get-std-details-withfilter", getStudentsDetailsWithFilters); //query parameters
+router.put("/put-students/:id", updateStudents); //put method
+router.put("/update-students-status", UpdateStudentsStatus); //put method to update multiple documents
+router.delete("/delete-student-byid/:userid", deleteStudentById); //delete method
+router.delete("/delete-student-byid2/:userid", deleteStudentById2); //delete method using findByIdAndDelete
+router.delete("/delete-student-many", deleteStudentMany); //delete method to delete multiple documents
 
 export default router;
